61009eb40ac3058f552a528e0f2f16c22d31d6f8
AugustHjortholm <s215800@dtu.dk>
2023-03-06 16:03:46 +0100

    Changed JSX file to TSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,163 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+//replace with orders
+const PRODUCTSTOTAL = [
+  {category: "Fruits", price: 1, stocked: true, name: "Apple"},
+  {category: "Fruits", price: 2, stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: 6, stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: 31, stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: 1, stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: 2, stocked: true, name: "Peas"}
+];
 
-function App() {
-  const [count, setCount] = useState(0)
+//replace URLs with more permanent solution
+const DELIVERYCOSTS = [
+  {name: "GLS", 
+  price: 42, 
+  img: "https://seekvectorlogo.com/wp-content/uploads/2022/01/general-logistics-systems-gls-vector-logo-2022.png", 
+  altText: "GLS", 
+  description: "Valgfri pakkeshop 1-2 hverdage"}, 
+  {name: "Bring", 
+  price: 46, 
+  img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Bring_logo.svg/1280px-Bring_logo.svg.png", 
+  altText: "Bring", 
+  description: "Valgfri pakkeshop 1-2 hverdage"}, 
+  {name: "Plante Land", 
+  price: 0, 
+  img: "https://seekvectorlogo.com/wp-content/uploads/2022/01/general-logistics-systems-gls-vector-logo-2022.png", 
+  altText: "Plante Land", 
+  description: "Hent i butik"}
+]
+
+/*
+function ProductRow({product}) {
+
+  function handleRemoveClick(){
+    document.getElementById(product.name).style.visibility = "hidden";
+  }
+
+  function handleAddQuantityClick(){
+    product.amount++;
+    document.getElementById(product.name + "amount").innerHTML = product.amount + " stk";
+  }
+  function handleRemoveQuantityClick(){
+    product.amount--;
+    document.getElementById(product.name + "amount").innerHTML = product.amount + " stk";
+  }
+  return (
+    <tr id = {product.name}>
+      <h1>{product.name + " "}</h1>
+      <tr>
+        <td><button onClick={handleAddQuantityClick}>+</button></td>
+        <h4 id = {product.name + "amount"}>{product.amount + " stk"}</h4>
+        <td><button onClick={handleRemoveQuantityClick}>-</button></td>
+      </tr>
+      <td><h2>{product.price}</h2></td>
+      <td><button onClick={handleRemoveClick}>🗑️</button></td>
+    </tr>
+  );
+}
+*/
+
+/*
+function ProductTable<Type>(arr: Type[]) {
+  const rows = [];
+  products.forEach((product) => {
+    rows.push(
+      <ProductRow
+        product={product}/>
+    );
+       });
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <table>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+  
+}
+*/
+
+
+
+const PRODUCTSCATEGORY = [
+  {price: "40 kr", amount:2, name: "Rose"},
+  {price: "35 kr", amount:1, name: "Scheffler"},
+  {price: "120 kr", amount:4, name: "Pions"},
+];
+
+//need to remove centering once its included in css
+function Title(){
+  return(
+  <div><h1><center>Ordreoversigt</center></h1></div>
+  )
+}
+function Subtotal(){
+  let prodcutPrice = 0
+  PRODUCTSTOTAL.forEach(element => {
+    prodcutPrice += element.price;
+  });
+  let subtotal = "Subtotal: " + prodcutPrice;
+  return(
+  <div><h2>{subtotal}</h2></div>
   )
 }
 
-export default App
+//TODO need to include the option to choose
+function DeliveryCost(){
+  let deliveryPrice
+  deliveryPrice = "Levering: " + DELIVERYCOSTS[0].price
+  return(
+  <div><h2>{deliveryPrice}</h2></div>
+  )
+}
+
+function DeliveryMethods(){
+  const list = DELIVERYCOSTS.map (method =>
+  <li key={method.name}>
+  <img
+  src={method.img}
+  alt={method.altText} 
+  width="100"/>
+  <p>
+    {method.description}
+  </p>
+
+</li>);
+  return (
+    <article>
+        <ul>{list}</ul>
+    </article>
+  )
+}
+
+function Total(){
+  let prodcutPrice = 0
+  PRODUCTSTOTAL.forEach(element => {
+    prodcutPrice += element.price;
+  });
+  let deliveryPrice = DELIVERYCOSTS[0].price
+  let totalPrice = prodcutPrice + deliveryPrice
+  let total = "Pris i alt: " + totalPrice
+  return(
+      <div><h1>{total}</h1><button>Til kassen</button></div>
+  )
+}
+
+function Summary(){
+  return (
+    <section>
+  <Title />
+  <Subtotal />
+  <DeliveryCost />
+  <DeliveryMethods />
+  <Total />
+  </section>
+  )
+}
+
+export default function App() {
+  return (
+    <section>
+  <Summary/>
+  </section>
+    );
+}
