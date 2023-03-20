d907253bafd01f529e5f2c00ab7c3a652d1eea63
Rikke GALLØE <rikke.galloe@soprasteria.com>
2023-03-20 16:17:05 +0100

    put address in DeliveryInformationpage

--- src/pages/DeliveryInformation.tsx
+++ src/pages/DeliveryInformation.tsx
@@ -1,10 +1,13 @@
 // TODO implement it as a form with data validation
 
+import { AddressForms } from "../components/AdressForms"
+
 
 export const DeliveryInformation = () => {
 
     return (
         <>
+            <AddressForms />
         </>
     )
 }
