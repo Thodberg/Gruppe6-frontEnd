3042d4a481a5f1dcbd7df2e283f924e7f846e9bb
Thodberg <jensjakob@thodberg.com>
2023-03-23 13:05:59 +0100

    kommentar

---
+++ src/test/App.test.tsx
@@ -0,0 +1,30 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+import App from "../App";
+
+describe(App.name, () => {
+  it("should render", () => {
+    render(<App />);
+    //expect(screen.getByLabelText("Artist name:")).toBeInTheDocument();
+    expect(screen.getByText("Planteland")).toBeInTheDocument();
+    expect(screen.getByText("Ordreoversigt")).toBeInTheDocument();
+    expect(screen.getByText("Subtotal")).toBeInTheDocument();
+    expect(screen.getByText("Levering")).toBeInTheDocument();
+    expect(screen.getByText("Ordreoversigt")).toBeInTheDocument();
+    
+  });
+});
+
+describe(App.name, () => {
+  it("should render", () => {
+    render(<App />);
+    //expect(screen.getByLabelText("Artist name:")).toBeInTheDocument();
+    expect(screen.getByText("Planteland")).toBeInTheDocument();
+    expect(screen.getByText("Ordreoversigt")).toBeInTheDocument();
+    expect(screen.getByText("Subtotal")).toBeInTheDocument();
+    expect(screen.getByText("Levering")).toBeInTheDocument();
+    expect(screen.getByText("Ordreoversigt")).toBeInTheDocument();
+    
+  });
+});
