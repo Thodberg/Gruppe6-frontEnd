d714f0dd471c7be61a3904b6f0115c86cf674b0e
Rikke GALLØE <rikke.galloe@soprasteria.com>
2023-03-27 13:45:31 +0200

    mere leveringsside

--- src/pages/DeliveryInformation.tsx
+++ src/pages/DeliveryInformation.tsx
@@ -2,12 +2,33 @@
 
 import { AddressForms } from "../components/AdressForms"
 
-
 export const DeliveryInformation = () => {
 
     return (
-        <>
-            <AddressForms />
-        </>
+        <div>
+            <h4>
+                Vi mangler kun det sidste
+            </h4>
+
+            <div>
+                <AddressForms />
+            </div>¨
+            <br />
+            <div id='dialog-window'>
+                <div id='scrollable-content'>
+                    En masse vilkår for køb kan puttes her. Nu bare dummy tekst så man kan se scrolle virker.
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum, earum facere. Eum deserunt esse necessitatibus ipsum doloremque quos expedita odit porro quisquam veritatis deleniti veniam, omnis voluptate repellat ipsam explicabo.</p>
+                    <label>
+                        <input type="checkbox" id="terms" name="terms" /> Jeg accepterer vilkår
+                    </label>
+                </div>
+            </div>
+            <br />
+            <div className='theme-c'>
+                <label>
+                    <input type="checkbox" id="terms" name="terms" /> Jeg vil gerne modtage nyhedsbrev og tilbud
+                </label>
+            </div>
+        </div>
     )
 }
