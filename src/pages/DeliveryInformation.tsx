da226106e61e3804025a9686aa48efa44f942cef
Rikke GALLØE <rikke.galloe@soprasteria.com>
2023-04-04 17:55:36 +0200

    implemented alerts for accepting terms

--- src/pages/DeliveryInformation.tsx
+++ src/pages/DeliveryInformation.tsx
@@ -1,13 +1,34 @@
 // TODO implement it as a form with data validation
 
+import { boolean } from "yargs"
 import { AddressForms } from "../components/AdressForms"
+import { useState } from "react";
 
 export const DeliveryInformation = () => {
+    const [termsAccepted, setTermsAccepted] = useState<boolean>(false);
+
+    function isChecked() {
+        if (termsAccepted === true) {
+            setTermsAccepted(false)
+        } else {
+            setTermsAccepted(true)
+        }
+    }
+
+    const verifyAll = () => {
+        // Add verification of leveringsinformationer er udfyldt
+        if (termsAccepted === true) {
+            alert("Alt er udfyldt du føres til betalingssiden. (ikke implementeret)")
+        }
+        else if (termsAccepted === false) {
+            alert("Venligst accepter vilkår og betingelser. Du finder knappen nederst.")
+        }
+    }
 
     return (
         <div>
             <h4>
-                Vi mangler kun de sidste informationer
+                Leveringsinformationer:
             </h4>
 
             <div>
@@ -19,7 +40,7 @@ export const DeliveryInformation = () => {
                     En masse vilkår for køb kan puttes her. Nu bare dummy tekst så man kan se scrolle virker.
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum, earum facere. Eum deserunt esse necessitatibus ipsum doloremque quos expedita odit porro quisquam veritatis deleniti veniam, omnis voluptate repellat ipsam explicabo.</p>
                     <label>
-                        <input type="checkbox" id="terms" name="terms" /> Jeg accepterer vilkår
+                        <input type="checkbox" id="terms" name="terms" onChange={isChecked} /> Jeg accepterer vilkår
                     </label>
                 </div>
             </div>
@@ -29,6 +50,7 @@ export const DeliveryInformation = () => {
                     <input type="checkbox" id="terms" name="terms" /> Jeg vil gerne modtage nyhedsbrev og tilbud
                 </label>
             </div>
+            <button onClick={verifyAll}>Til betaling</button>
         </div>
     )
 }
