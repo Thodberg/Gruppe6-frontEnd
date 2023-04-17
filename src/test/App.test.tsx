8791ac3dba01ab6f1520c05a22ffda5dd78efde8
Jens Jakob Thodberg <jensjakob@thodberg.com>
2023-04-17 12:24:02 +0200

    overflødig App.test.tsx blev fjernet og mindre ændring i tilbageværende App.test.tsx

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -3,22 +3,16 @@ import { render, screen } from "@testing-library/react";
 
 import App from "../App";
 
-// describe(App.name, () => {
-//   it("should render", () => {
-//     render(<App />);
-//     //expect(screen.getByLabelText("Artist name:")).toBeInTheDocument();
-//     expect(screen.getByText("Planteland")).toBeInTheDocument();
-//     expect(screen.getByText("Ordreoversigt")).toBeInTheDocument();
-//     expect(screen.getByText("Subtotal")).toBeInTheDocument();
-//     expect(screen.getByText("Levering")).toBeInTheDocument();
-//     expect(screen.getByText("Ordreoversigt")).toBeInTheDocument();
-
-//   });
-// });
-
-describe('App', () => {
-  it('renders App component', () => {
+describe(App.name, () => {
+  it("should render", () => {
     render(<App />);
-    screen.debug();
-  })
-})
+    //expect(screen.getByLabelText("Artist name:")).toBeInTheDocument();
+    expect(screen.getByText("Planteland")).toBeInTheDocument();
+    expect(screen.getByText("Ordreoversigt")).toBeInTheDocument();
+    expect(screen.getByText("Subtotal")).toBeInTheDocument();
+    expect(screen.getByText("Levering")).toBeInTheDocument();
+    expect(screen.getByText("Ordreoversigt")).toBeInTheDocument();
+    expect(screen.getByRole('radio', { name: 'IPv6' }));
+    
+  });
+});
